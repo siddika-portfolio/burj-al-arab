39e8bc6ddf79caeab14fa56b9975db7c0cde1291
Siddika-akhter <siddika.tas28@gmail.com>
2020-09-29 20:19:29 +0600

    burj al arab first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBF5IY_N444JKBnCfh-0rvkd51uayM-h-g",
+    authDomain: "burj-al-arab-ea6da.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-ea6da.firebaseio.com",
+    projectId: "burj-al-arab-ea6da",
+    storageBucket: "burj-al-arab-ea6da.appspot.com",
+    messagingSenderId: "281500192500",
+    appId: "1:281500192500:web:8710e6cec3b18f11fe18a1"
+  };
+
+
+  export default firebaseConfig;
