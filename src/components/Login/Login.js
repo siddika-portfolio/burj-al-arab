8987a838b98565193de6f7e3b7e16a7a15782023
Siddika-akhter <siddika.tas28@gmail.com>
2020-10-01 10:45:23 +0600

    booking component push

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,6 +21,7 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email}
             setLoggedInUser(signedInUser);
+            storeAuthToken();
             history.replace(from);
             
           }).catch(function(error) {
@@ -30,6 +31,16 @@ const Login = () => {
           
           });
     }
+
+const storeAuthToken = () => {
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+    .then(function(idToken) {
+       sessionStorage.setItem('token', idToken)
+      }).catch(function(error) {
+        // Handle error
+      });
+}
+
     return (
         <div>
             <h1>This is Login</h1>
